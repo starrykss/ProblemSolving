601029ef37354e769ab11cf6072c3228b9420d45
starrykss <kss34891@gmail.com>
2022-12-01 21:58:12 +0900

    [단계별로 풀어보기] > [동적 계획법 1]

--- Codes/BOJ14889.cpp
+++ Codes/BOJ14889.cpp
@@ -4,15 +4,15 @@
 using namespace std;
 
 int N, ans(INT_MAX);
-int stats[20][20], team1[10], team2[10];
-int pick[20];
+int S[20][20], start[10], link[10];
+bool pick[20];
 
 void Input() {
     cin >> N;
     
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++) {
-            cin >> stats[i][j];
+            cin >> S[i][j];
         }
     }
 }
@@ -22,41 +22,41 @@ void Output() {
 }
 
 void Update() {
-    int team1_size = 0, team2_size = 0;
+    int sizeS = 0, sizeL = 0;
     for (int i = 0; i < N; i++) {
-        if (pick[i] == 0) {
-            team1[team1_size++] = i;
+        if (pick[i] == false) {    // 선택되지 않은 사람들은 start 팀에 넣기
+            start[sizeS++] = i;
         }
-        else {
-            team2[team2_size++] = i;
+        else {    // 선택된 사람들은 link 팀에 넣기
+            link[sizeL++] = i;
         }
     }
 
-    int sum_1 = 0, sum_2 = 0;
+    int sumS = 0, sumL = 0;
     for (int i = 0; i < N / 2; i++) {
         for (int j = i + 1; j < N / 2; j++) {
-            sum_1 += stats[team1[i]][team1[j]] + stats[team1[j]][team1[i]];
-            sum_2 += stats[team2[i]][team2[j]] + stats[team2[j]][team2[i]];
+            sumS += S[start[i]][start[j]] + S[start[j]][start[i]];
+            sumL += S[link[i]][link[j]] + S[link[j]][link[i]];
         }
     }
-    if (ans > abs(sum_1 - sum_2)) {
-        ans = abs(sum_1 - sum_2);
+
+    if (ans > abs(sumS - sumL)) {    // 최솟값 찾기
+        ans = abs(sumS - sumL);
     }
 }
 
-void DFS(int cur, int pick_count) {
-    if (pick_count == (N / 2)) {
+void DFS(int start, int cnt) {
+    if (cnt == N / 2) {
         Update();
         return;
     }
 
-    for (int i = cur; i < N; i++) {
-        pick[i] = 1;
-        DFS(i + 1, pick_count + 1);
-        pick[i] = 0;
+    for (int i = start; i < N; i++) {
+        pick[i] = true;
+        DFS(i + 1, cnt + 1);
+        pick[i] = false;
     }
 }
-
 
 int main() {
     ios_base::sync_with_stdio(false);
