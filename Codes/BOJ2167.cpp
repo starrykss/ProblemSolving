abd35784ee8e25de10710d2f83169d6cbae2d946
starrykss <kss34891@naver.com>
2023-01-23 19:38:16 +0100

    [단계별로 풀어보기] > [누적합] + 2차원 배열에서의 누적 합 문제

---
+++ Codes/BOJ2167.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int N, M, K;
+int matrix[1025][1025];
+int pSum[1025][1025];
+int x1, x2, y1, y2; 
+
+void Solution() {
+    cin >> N >> M;
+
+    // 표 채우기
+    for (int r = 1; r <= N; r++) {
+        for (int c = 1; c <= M; c++) {
+            cin >> matrix[r][c];
+            pSum[r][c] = pSum[r - 1][c] + pSum[r][c - 1] - pSum[r - 1][c - 1] + matrix[r][c];
+        }
+    }
+
+    cin >> K;
+
+    // 구해야 하는 좌표 입력 받기 : (x1, y1), (x2, y2)
+    for (int i = 1; i <= K; i++) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        
+        cout << pSum[x2][y2] - (pSum[x1 - 1][y2] + pSum[x2][y1 - 1]) + pSum[x1 - 1][y1 - 1] << '\n';
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Solution();
+
+    return 0;
+}
