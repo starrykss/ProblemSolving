2ed2495df99a1077300c4eb4a9eaa3d9b6c65ae8
starrykss <kss34891@naver.com>
2023-02-05 16:44:18 +0100

    [단계별로 풀어보기] > [그리디 알고리즘]

---
+++ Codes/BOJ11047.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int N, K, value, cnt;
+vector<int> values;
+
+void Input() {
+    cin >> N >> K;
+    for (int i = 1; i <= N; i++) {
+        cin >> value;
+        values.push_back(value);
+    }
+}
+
+int Solution(int k) {
+    sort(values.begin(), values.end(), greater<int>());    // 내림차순 정렬
+
+    int choose;
+    for (auto i : values) {
+        if (i <= k) {
+            // cout << "[i] : " << i << " [k / i] : " << k / i << " ";
+            cnt += (k / i);
+            // cout << "[cnt] : " << cnt << " ";
+            k -= ((k / i) * i);
+            // cout << "[k] : " << k << '\n';
+        }
+    }
+    return cnt;
+}
+
+void Output() {
+    cout << Solution(K) << '\n';
+}
+
+void Solve() {
+    Input();
+    Output();
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    Solve();
+
+    return 0;
+}
